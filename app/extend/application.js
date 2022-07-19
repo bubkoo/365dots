d70a5736d6d1da842d8a923185806284a0fea68c
bubkoo <bubkoo.wy@gmail.com>
2022-07-19 13:45:21 +0800

    fix: cache

--- app/extend/application.js
+++ app/extend/application.js
@@ -1,7 +1,15 @@
-const cache = {};
+let cacheByDate = {};
 
 module.exports = {
   cache(search, svg) {
+    const now = new Date();
+    const date = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
+    if (!cacheByDate[date]) {
+      cacheByDate = {};
+      cacheByDate[date] = {};
+    }
+
+    const cache = cacheByDate[date];
     if (svg) {
       cache[search] = svg;
     } else {
