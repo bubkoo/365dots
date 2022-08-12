2e774ea6c9de2b661e4627c130c53c601242cd4c
bubkoo <bubkoo.wy@gmail.com>
2022-08-12 10:51:08 +0800

    feat: add cache-control

--- app/controller/home.js
+++ app/controller/home.js
@@ -17,6 +17,7 @@ class HomeController extends Controller {
       app.cache(ctx.search, cache);
     }
 
+    ctx.headers['cache-control'] = 'max-age=43200';
     ctx.type = 'image/svg+xml';
     ctx.body = cache;
   }
